f4a3afe8d1d1e32ba6be8dd8a1ed37943c144201
jaehyeok <ruyshyttr@gmail.com>
2025-12-01 14:53:35 +0900

    Triangle Draw

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -1,10 +1,120 @@
 ﻿#include <iostream>
+#include <string>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 // Window dimensions
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
+
+GLuint VAO, VBO, shader;
+
+// Vertex Shader
+static const char* vShader = "												\n\
+#version 330																	\n\
+																					\n\
+layout (location = 0) in vec3 pos;											\n\
+																					\n\
+void main()																		\n\
+{																					\n\
+	gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);			\n\
+}";
+
+static const char* fShader = "												\n\
+#version 330																	\n\
+																					\n\
+out vec4 color;																	\n\
+																					\n\
+void main()																		\n\
+{																					\n\
+	color = vec4(1.0, 0.0, 0.0, 1.0);											\n\
+}";
+
+void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+{
+	GLuint theShader = glCreateShader(shaderType);
+
+	const GLchar* theCode[1];
+	theCode[0] = shaderCode;
+
+	GLint codeLength[1];
+	codeLength[0] = strlen(shaderCode);
+
+	glShaderSource(theShader, 1, theCode, codeLength);
+	glCompileShader(theShader);
+
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(shader, sizeof(eLog), NULL, eLog);
+		std::cout << "Error compiling the" << shaderType << "shader:: " << eLog << std::endl;
+		return;
+	}
+
+	glAttachShader(theProgram, theShader);
+}
+
+void CompileShaders()
+{
+	shader = glCreateProgram();
+
+	if (!shader)
+	{
+		std::cout << "Error creating shader program!" << std::endl;
+		return;
+	}
+
+	AddShader(shader, vShader, GL_VERTEX_SHADER);
+	AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	glLinkProgram(shader);
+	glGetProgramiv(shader, GL_LINK_STATUS, &result);
+	if (!result)
+	{
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		std::cout << "Error linking program : " << eLog << std::endl;
+		return;
+	}
+
+	glValidateProgram(shader);
+	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+	if (!result)
+	{
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		std::cout << "Error validating program : " << eLog << std::endl;
+		return;
+	}
+}
+
+void CreateTriangle()
+{
+	GLfloat vertices[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+}
 
 int main()
 {
@@ -20,7 +130,9 @@ int main()
 	// OpenGL version
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Core profile = No Backwards compatibility
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Allow forward compatibility
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "test window", NULL, NULL);
@@ -37,17 +149,20 @@ int main()
 
 	// Set Context for GLEW to use
 	glfwMakeContextCurrent(mainWindow);
-	
+
 	// Allow modern extension features
 	glewExperimental = GL_TRUE;
 
-	if(glewInit() != GLEW_OK)
+	if (glewInit() != GLEW_OK)
 	{
 		std::cout << "GLEW initialistion failed!" << std::endl;
 		glfwDestroyWindow(mainWindow);
 		glfwTerminate();
 		return 1;
 	}
+
+	CreateTriangle();
+	CompileShaders();
 
 	// Setup Viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
@@ -61,6 +176,14 @@ int main()
 		// Clear Window
 		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shader);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+		glUseProgram(0);
 
 		glfwSwapBuffers(mainWindow);
 
