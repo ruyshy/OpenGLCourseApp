54e6790e69b54fe0a5d6a05475588105b987968a
jaehyeok <ruyshyttr@gmail.com>
2025-12-09 13:49:45 +0900

    glm

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -1,31 +1,36 @@
 ﻿#include <iostream>
 #include <string>
+#include <stdlib.h>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <stdlib.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 // Window dimensions
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
 
-GLuint VAO, VBO, shader, uniformXMove;
+GLuint VAO, VBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
 float triMaxoffset = 0.7f;
-float triIncrement = 0.0005f;
+float triIncrement = 0.005f;
 
 // Vertex Shader
-static const char* vShader = "												\n\
-#version 330																	\n\
-																					\n\
-layout (location = 0) in vec3 pos;											\n\
-																					\n\
-uniform float xMove;															\n\
-																					\n\
-void main()																		\n\
-{																					\n\
-	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);				\n\
+static const char* vShader = "														\n\
+#version 330																			\n\
+																							\n\
+layout (location = 0) in vec3 pos;													\n\
+																							\n\
+uniform mat4 model;																	\n\
+																							\n\
+void main()																				\n\
+{																							\n\
+	gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);			\n\
 }";
 
 static const char* fShader = "												\n\
@@ -99,7 +104,7 @@ void CompileShaders()
 		return;
 	}
 
-	uniformXMove = glGetUniformLocation(shader, "xMove");
+	uniformModel = glGetUniformLocation(shader, "model");
 }
 
 void CreateTriangle()
@@ -202,7 +207,10 @@ int main()
 
 		glUseProgram(shader);
 
-		glUniform1f(uniformXMove, triOffset);
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
