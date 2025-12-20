82b3b00744151e22c848bbeb8f9c8a8ff44b9856
jaehyeok <ruyshyttr@gmail.com>
2025-12-20 13:50:12 +0900

    projection

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -13,7 +13,7 @@
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
 
-GLuint VAO, VBO, IBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel, uniformProjection;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -36,10 +36,11 @@ layout (location = 0) in vec3 pos;													\n\
 																							\n\
 out vec4	vCol;																			\n\
 uniform mat4 model;																	\n\
+uniform mat4 projection;															\n\
 																							\n\
 void main()																				\n\
 {																							\n\
-	gl_Position = model * vec4(pos, 1.0);											\n\
+	gl_Position = projection * model * vec4(pos, 1.0);							\n\
 	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);										\n\
 }";
 
@@ -115,6 +116,7 @@ void CompileShaders()
 	}
 
 	uniformModel = glGetUniformLocation(shader, "model");
+	uniformProjection = glGetUniformLocation(shader, "projection");
 }
 
 void CreateTriangle()
@@ -200,13 +202,15 @@ int main()
 		return 1;
 	}
 
-	CreateTriangle();
-	CompileShaders();
-
 	glEnable(GL_DEPTH_TEST);
 
 	// Setup Viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	CreateTriangle();
+	CompileShaders();
+
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)bufferWidth / (GLfloat)bufferHeight, 0.1f, 100.0f);
 
 	// Loop untill window closed
 	while (!glfwWindowShouldClose(mainWindow))
@@ -252,11 +256,12 @@ int main()
 		glUseProgram(shader);
 
 		glm::mat4 model(1.0f);
-		//model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.5f));
 		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
 		glBindVertexArray(VAO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
